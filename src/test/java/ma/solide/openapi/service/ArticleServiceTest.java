48bc6067ff21aeef78feea22a4c3f9d9cbb55be8
barssim <qarfaoui@gmail.com>
2025-06-13 11:07:11 +0200

    test ArticleService

--- src/test/java/ma/solide/openapi/service/ArticleServiceTest.java
+++ src/test/java/ma/solide/openapi/service/ArticleServiceTest.java
@@ -1,47 +1,46 @@
-//package ma.solide.openapi.service;
-//
-//import ma.solide.openAPI.model.ArticleDto;
-//import ma.solide.openapi.model.Article;
-//import ma.solide.openapi.model.ArticleMapper;
-//import ma.solide.openapi.repository.ArticleRepository;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.Mockito.*;
-//import ma.solide.openapi.service.ArticleService;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//
-//import java.util.Optional;
-//
-//class ArticleServiceTest {
-//
-//    @Mock
-//    private ArticleRepository articleRepository;
-//
-//    @InjectMocks
-//    private ArticleService articleService;
-//
-//    @BeforeEach
-//    void setUp() {
-//        MockitoAnnotations.openMocks(this);
-//    }
-//
-//    @Test
-//    void testSaveArticle() {
-//        // Given
-//        Article myArticle = new Article(4L, "AutoTeil");
-//        when(articleRepository.save(any(Article.class))).thenReturn(myArticle);
-//
-//        // When
-//        ArticleDto savedArticle = articleService.createArticle(ArticleMapper.toDto(Optional.of(myArticle)));
-//
-//        // Then
-//        assertNotNull(savedArticle.getId());
-//        assertEquals("AutoTeil", savedArticle.getName());
-//        verify(articleRepository, times(1)).save(myArticle);
-//    }
-//}
+package ma.solide.openapi.service;
+
+import ma.solide.openAPI.model.ArticleDto;
+import ma.solide.openapi.model.Article;
+import ma.solide.openapi.model.ArticleMapper;
+import ma.solide.openapi.repository.ArticleRepository;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+class ArticleServiceTest {
+
+    @Mock
+    private ArticleRepository articleRepository;
+
+    @InjectMocks
+    private ArticleService articleService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testSaveArticle() {
+        // Given
+        Article myArticle = new Article(4L, "AutoTeil");
+        ArticleDto articleDto = ArticleMapper.toDto(Optional.of(myArticle));
+
+        when(articleRepository.save(any(Article.class))).thenReturn(myArticle);
+
+        ArticleDto savedArticle = articleService.createArticle(articleDto);
+
+        assertEquals("AutoTeil", savedArticle.getName());
+        verify(articleRepository, times(1)).save(any(Article.class));
+    }
+
+}
